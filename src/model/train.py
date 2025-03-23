8082a41097d441b85611bce1b78b6bba48ba64f0
pradnya-git-dev <kandarkarp@gmail.com>
2025-03-23 14:40:49 -0400

    challenge04

--- src/model/train.py
+++ src/model/train.py
@@ -9,11 +9,11 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+
 # define functions
 def main(args):
     # TO DO: enable autologging
     mlflow.autolog()
-
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -36,7 +36,8 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 
+               'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
@@ -60,6 +61,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
